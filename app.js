d84da9d9b295049ee3db0061f31ce1b8164a16d9
Iskandar Agung <iskand.agung@gmail.com>
2022-09-16 07:07:35 +0700

    deployment test

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
